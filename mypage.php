89e2cb9bbdd4df3a3be0f17b09bf33b648a2e6ef
Kento Tsuno <jinyejianren@gmail.com>
2020-06-22 16:00:51 +0900

    マイページの追加

---
+++ mypage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>マイページ</title>
+    <link rel="stylesheet" href="mypage.css">
+</head>
+<body>
+    <div class="mypage-wrapper">
+
+        <div class="info">
+            <h2>プロフィール</h2>
+            <table>
+                <tr>
+                    <th class="right">名前</th>
+                    <td>テスト太郎</td>
+                </tr>
+                <tr>
+                    <th>バイクの車種</th>
+                    <td>CB1000R</td>
+            </table>
+            <h3>お気に入りの画像</h3>
+            <img src="https://d1uzk9o9cg136f.cloudfront.net/f/16782548/rc/2020/06/16/3e9aa23ea59511bfa4d77e5022dee5584ddc8bd5_xlarge.jpg" alt="バイク">
+
+        </div>
+
+        <div class="favedit">
+
+            <h2>投稿した場所</h2>
+            <div class="favEdit1">
+
+                <h3>琵琶湖最高!!</h3>
+                <p>風が気持ち良い快適</p>
+                <button>編集</button>
+                <button>削除</button>
+
+            </div>
+            <div class="favEdit2">
+
+                <h3>京都タワー高い!!</h3>
+                <p>近くで見るとすごい</p>
+                <button>編集</button>
+                <button>削除</button>
+
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
