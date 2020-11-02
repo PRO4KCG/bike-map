a973080cf3304eb7951eb6eb4c88424be35c49b1
kitamura <st061702@m02.kyoto-kcg.ac.jp>
2020-11-02 12:11:22 +0900

    複数出てきた時の天気予報の処理

--- public/js/Mapping.js
+++ public/js/Mapping.js
@@ -1,28 +1,18 @@
-//const { result } = require("lodash");
+
 
 var map = null;
 
 
 const mapon = () =>{//html読み込み時にjsを処理
     map = L.map('mapid').setView([37.985358, 135.753331], 5);
-    /*
-    var routing = document.querySelector(".Routing");
-    var startLocX = null;
-    var startLocY = null;
-    var endLocX = null;
-    var endLocY = null;
-    */
     let DestLat;
     let DestLon;
-    //let DestName = DestinationName;
     let markers = [];
     var weatherHTML = document.querySelector(".weather");
     let MeLocationLat = null;
     let MeLocationLon = null;
 
-    //var Ygeocoder = new Y.GeoCoder();
 
-    //var start = document.querySelector(".start");
 
     var NaviStart = document.querySelector(".btn-primary");
     var getlocate = document.querySelector("#GetLocate");
@@ -37,7 +27,6 @@ const mapon = () =>{//html読み込み時にjsを処理
         DestLon = dest[i]["longitude"];
         markers[i] = L.marker([DestLat, DestLon]).addTo(map).on('click',function(e) { clickEvent(e); } );
         markers[i].bindPopup(L.popup({
-            //closeOnClick: true,
             autoClose: false
         }).setContent(dest[i]["locationName"])).openPopup();
         markers[i].id = dest[i]["locationID"];
@@ -49,14 +38,11 @@ const mapon = () =>{//html読み込み時にjsを処理
     
     //クリックイベントで発火する関数
     function clickEvent(e){
-        //alert( e.target.latitude + ": "+ e.target.longitude);
         window.location = "/search/"+e.target.id;
         alert(e.target.name + "が選択されました");
         
         DestLat = e.target.latitude;
         DestLon = e.target.longitude;
-        console.log("number:"+e.target.number);
-        console.log(markers);
 
         let currentMarkers = [];
 
@@ -64,24 +50,14 @@ const mapon = () =>{//html読み込み時にjsを処理
             map.removeLayer(markers[i]);
             if(e.target.number == i) {
                 currentMarkers = markers[i]
-            }//console.log(test);
+            }
     
         }
         let marker = L.marker([currentMarkers.latitude, currentMarkers.longitude]).addTo(map);
         marker.bindPopup(currentMarkers.name).openPopup();
-        //map.removeLayer(markers[e.target.number]);
-        //marker = L.marker([DestLat, DestLon]).addTo(map);
-        //marker.bindPopup(e.target.name).openPopup();
-        //map.panTo(e.latlng);
         
     }
-
     
-
-    //OpenWeatherMap天気予報API
-    //var owm_key = "39e545646eebdf1e60bcc141aa8fa102";
-
-    console.log("配列長さ："+markers.length);
     if(markers.length == 1){
     fetch('http://api.openweathermap.org/data/2.5/onecall?lat='+DestLat+'&lon='+DestLon+'&lang=ja&APPID=39e545646eebdf1e60bcc141aa8fa102')
     .then(function(response){
@@ -89,7 +65,6 @@ const mapon = () =>{//html読み込み時にjsを処理
     }).then(function(json){
         jsonArray = JSON.stringify(json);
         data = JSON.parse(jsonArray);
-        //console.log("json中身:"+JSON.stringify(json));
 
         for(var i = 0; i <= 6; i++){
 
@@ -111,51 +86,6 @@ const mapon = () =>{//html読み込み時にjsを処理
         }
     })
 }
-    /*
-     let marker = L.marker([DestLat, DestLon]).addTo(map);
-     marker.bindPopup(DestName).openPopup();
-    */
-
-    
-
-    //経路探索
-    /*
-    routing.onclick = () =>{
-        L.Routing.control({
-            waypoints: [
-                L.latLng(startLocY, startLocX),
-                L.latLng(endLocY, endLocX)
-            ],
-            routeWhileDragging: true
-        }).addTo(map);
-    }
-    */
-
-    /*
-    //住所から緯度経度
-    start.onclick = () =>{ 
-        var input_message = document.getElementById("input_messageS").value;
-            console.log(input_message)
-            var request = { query : input_message};
-
-            Ygeocoder.execute(request , function( ydf_data ){
-                if ( ydf_data.features.length > 0 ) {
-                    var latlng = ydf_data.features[0].latlng;
-                
-                    console.log("名称 : "+ ydf_data.features[0].name);
-                    console.log("緯度 : "+ latlng.lat());
-                    console.log("経度 : "+ latlng.lng());
-
-                    startLocY = latlng.lat()
-                    startLocX = latlng.lng()
-                  }
-            })
-            
-    }
-    */
-
-    
-    
 
    //ナビ開始
    NaviStart.onclick = () => {
@@ -191,8 +121,5 @@ function onLocationFound(e){
 
 
 }   
-   
-    //http://api.openweathermap.org/data/2.5/weather?lat={緯度}&lon={経度}&APPID={自分のAPIキー}
-    
 
 onload = mapon;
