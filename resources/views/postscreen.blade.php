7a084294e39aa6bb7d3c76227c8d4ef5aaa74caf
“takahashi <yuuto0442l16p@gmail.com>
2020-11-09 15:19:19 +0900

    frontの修正

--- resources/views/postscreen.blade.php
+++ resources/views/postscreen.blade.php
@@ -2,19 +2,29 @@
 
 @section('content')
 <div class="container">
-    <h2>ユーザー投稿</h2>
-    <a href="{{ url('/newpost') }}"><button type="button" class="btn btn-primary">投稿画面</button></a>
+    <div class="card">
+         <div class="card-body">
+    <h2><p>ユーザー投稿</p></h2>
+     
+   <p> <a href="{{ url('/newpost') }}"><button type="button" class="btn btn-primary ">投稿画面</button></a></p>
+
+</div>
+   </div>
     <div class="card">
         <div class="card-body">
             @foreach ($postResults as $post)
+            <li class="list-group-item">
                 <h4 class="card-title">{{ $post->title }}</h4>
                 <p class="card-text">{{ $post->comment }}</p>
                 <form action="/postscreen" method="post">
                     @csrf
-                    <button type="submit" class="card-link" name="like" value="{{ $post->favLocID }}">いいねボタン</button>
+                    <button type="submit" class="card-link btn text-danger " name="like" value="{{ $post->favLocID }}"><i class="fas fa-heart "></i>  <span>{{ $post->rating }}</span></button>
                 </form>
-                <p>{{ $post->rating }}</p>
+
+              
             @endforeach
+            </li>
+
         </div>
     </div>
 </div>
