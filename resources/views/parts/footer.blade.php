2dc154fbf01099bc189dde680d1e6d28c16a60cd
mouri0910 <yu09103071@yahoo.co.jp>
2020-12-19 15:04:31 +0900

    フッターにリンク配置２回目

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ url('/privacy') }}">プライバシーポリシー</a>
 <a href="{{ url('/contact') }}">お問い合わせ</a>
-<center><small>@2020 Bike-Map</small></center>
+<center><small>＠2020 Bike-Map</small></center>
